176db9e1a3f8f2f9ed1e1c02a7e3d00709c16084
braian3471 <luizflorea10@gmail.com>
2025-09-01 17:33:04 -0300

    Atualiza footer e add api github

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -1,4 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
+    // Toggle de seções de conhecimentos
     const botoes = [
         { botaoId: "linguagens", conteudoId: "conteudoLinguagens" },
         { botaoId: "ferramentas", conteudoId: "conteudoFerramentas" },
@@ -6,19 +7,40 @@ document.addEventListener("DOMContentLoaded", function () {
     ];
 
     const todosConteudos = botoes.map(item => document.getElementById(item.conteudoId));
-
+    // Adiciona evento de clique a cada botão
     botoes.forEach(({ botaoId, conteudoId }) => {
         const botao = document.getElementById(botaoId);
         const conteudo = document.getElementById(conteudoId);
 
         botao.addEventListener("click", function () {
-            // Esconde todos os outros conteúdos
+            
             todosConteudos.forEach(c => {
                 if (c !== conteudo) c.classList.remove("ativo");
             });
 
-            // Alterna só o conteúdo clicado
+            
             conteudo.classList.toggle("ativo");
         });
     });
+
+    
+
+    // api github
+    const username = "braian3471"; // coloque seu usuário GitHub
+    fetch(`https://api.github.com/users/${username}/repos`)
+    .then(response => response.json())
+    .then(repos => {
+        const container = document.getElementById('repositorios');
+        repos.forEach(repo => {
+            const div = document.createElement('div');
+            div.classList.add('card');
+            div.innerHTML = `
+                <h3>${repo.name}</h3>
+                <p>${repo.description || "Sem descrição"}</p>
+                <a href="${repo.html_url}" target="_blank">Ver no GitHub</a>
+            `;
+            container.appendChild(div);
+        });
+    });
+
 });
